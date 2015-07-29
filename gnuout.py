22ed714ab9913a47925fc86725faba9f78e5b940
minzastro <minzastro@localhost>
2015-07-29 16:55:51 +0200

    gnuout.py upgrade

--- gnuout.py
+++ gnuout.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import numpy as np
 import argparse
-import matplotlib
+import sys
 import matplotlib.pyplot as plt
 
 color_sequence = 'rbgmcy'
@@ -16,6 +16,9 @@ parser.add_argument('-x', type=str, default='1,2',
 parser.add_argument('-i', type=str, default=None,
                     help='Input file')
 
+parser.add_argument('-t', '--title', type=str, default=None,
+                    help='Plot title')
+
 parser.add_argument('-d', '--delimiter', type=str, default=None,
                     help='Input file')
 
@@ -28,22 +31,46 @@ parser.add_argument('--save', type=str, default=None,
 parser.add_argument('-l', '--log', type=str, default='',
                     help='x or y or xy for logscale')
 
+parser.add_argument('--header', action="store_true",
+                    default=False,
+                    help='use header')
+
+parser.add_argument('-lp', '--legend_position', type=str,
+                    default='lower center',
+                    help='legend position or none for no legend')
 
 args = parser.parse_args()
 
-if args.delimiter == None:
-    data = np.loadtxt(args.i)
+if args.i is None:
+    source = sys.stdin
 else:
-    data = np.loadtxt(args.i, delimiter=args.delimiter)
+    source = open(args.i, 'r')
 
+header = None
+if args.header:
+    if args.delimiter is not None:
+        header = source.readline().strip().split(args.delimiter)
+    else:
+        header = source.readline().strip().split()
+    skiprows = 1
+else:
+    skiprows = 0
+
+
+if args.delimiter is None:
+    data = np.loadtxt(source, skiprows=skiprows)
+else:
+    data = np.loadtxt(source, delimiter=args.delimiter, skiprows=skiprows)
 
 def get_color(icolor):
     while icolor >= len(color_sequence):
         icolor = icolor - len(color_sequence)
     return color_sequence[icolor]
 
+
 def get_style(icolor):
     return '%s%s' % (get_color(icolor), args.style)
+
 
 def convert_column(column):
     # TODO: Add verification here.
@@ -59,7 +86,11 @@ columns = []
 for colpar in args.x.split(','):
     columns.extend(convert_column(colpar))
 
-plt.title(args.i)
+if args.title is not None:
+    plt.title(args.title)
+else:
+    plt.title(args.i)
+
 if args.log == 'x':
     plt.semilogx()
 elif args.log == 'y':
@@ -67,10 +98,19 @@ elif args.log == 'y':
 elif args.log == 'xy':
     plt.loglog()
 
+labels = []
 for j, column in enumerate(columns[1:]):
     plt.plot(data[:, columns[0]-1], data[:, column-1], get_style(j))
+    if args.header:
+        labels.append(r'$%s$' % header[column - 1])
+        plt.xlabel(header[columns[0] - 1])
+    else:
+        labels.append('Column %s' % (column - 1))
+    if args.legend_position != 'none':
+        plt.legend(labels, loc=args.legend_position,
+                   fancybox=True, shadow=True, ncol=2)
 
-if args.save == None:
+if args.save is None:
     plt.show()
 else:
     plt.savefig(args.save, format='png')
